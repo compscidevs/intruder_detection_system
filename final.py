c7cff52c4bc29f54cc08287034356908f83de1ef
unknown <wkatswamba23@gmail.com>
2024-08-05 08:55:08 -0700

    allocated tensors

--- final.py
+++ final.py
@@ -1,1 +1,7 @@
-import cv2
+# Load TFLite model and allocate tensors
+interpreter = tf.lite.Interpreter(model_path="model/model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
