0ea2b248bbe76c830f2733743e9277025c4929ef
Okwii Yakub <okwiiyakub@gmail.com>
2024-08-05 18:27:00 +0300

    secon commit

--- final.py
+++ final.py
@@ -3,3 +3,12 @@ import serial.tools.list_ports
 import time
 import os
 import cv2
+import numpy as np
+import tensorflow as tf
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from picamera2 import Picamera2
+import RPi.GPIO as GPIO
